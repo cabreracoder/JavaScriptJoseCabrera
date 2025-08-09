6628e5f8eb3541b1c0537dd0683c9db756d665b8
cabreracoder <josectorres824@gmail.com>
2025-08-09 09:24:14 -0500

    operadores

---
+++ operadores.js
@@ -0,0 +1,35 @@
+//suma, resta, multiplicacion y division.
+//Calcular valores de dos o mas elementos.
+//Sumamos 2 numeros.
+let num1 =8;
+var num2 =4;
+let suma = num1+num2;
+console.log("El resultado de la suma es "+suma);
+//contadores.
+let edad =24;
+edad++;
+console.log("tu edad es :"+edad);
+//resta
+let res1=6;
+var res2=2;
+let resta= res1-res2;
+let edadSeñora=80;
+edadSeñora--;
+console.log("El resultado de la resta es : "+resta);
+console.log("La edad actual, despues de la operacion es:"+ edadSeñora);
+//Multiplicacion
+let mul1= 5;
+var mul2= 6;
+let multiplicacion= mul1*mul2;
+console.log("El resultado de la multiplicacion es : "+multiplicacion);
+//Division 
+let div1 = 20;
+var div2 = 5;
+let division = div1/div2;
+console.log("El resultado de la division es : "+division);
+//Residuo 
+let rsd1= 15;
+var rsd2= 4;
+let residuo = rsd1 % rsd2;
+console.log("El resultado del residuo es : "+residuo);
+
---
+++ variablescompletas.js
@@ -0,0 +1,18 @@
+// String
+var sena ="ADSO";
+console.log(sena);
+//numeros
+let numeroEntero =17;
+var numeroNegativo = -4;
+let numeroDecimal = 11.74;
+//booleanos true-false
+var estoyEstudiando = true;
+let estoyAburrido = false;
+//undefined
+var num;
+console.log(num);
+var num =0;
+console.log(num);
+//null
+var sinNada= null;
+console.log(sinNada);
