d725991103471ed3342b9b8d446a3f8c7cd35548
cabreracoder <josectorres824@gmail.com>
2025-08-09 12:03:25 -0500

    sumaDinamica

---
+++ sumaDinamica.js
@@ -0,0 +1,7 @@
+let num1 = prompt ("Digita el primer numero");
+num1 =parseInt (num1)
+let num2 = prompt ("Digita el segundo numero");
+num2 =parseInt (num2)
+let suma = num1+num2;
+console.log("la suma de los numeros es: " + suma);
+alert("Este es el resultado de la suma " + suma);
