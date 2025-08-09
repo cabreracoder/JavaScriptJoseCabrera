330c9d01a2f26b2169326e2d8ed0e84aefeca2e8
cabreracoder <josectorres824@gmail.com>
2025-08-09 11:30:34 -0500

    prompt

---
+++ prompt.js
@@ -0,0 +1,5 @@
+let palabra1 = prompt("Digite palabra a concatenar");
+let palabra2 = prompt("Digite la palabra 2 a concatenar");
+let concatenar = palabra1 + palabra2;
+console.log(concatenar);
+alert("ESTA es la palabra concatenada " + concatenar);
